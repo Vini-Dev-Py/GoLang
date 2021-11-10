83905f7850058b28d21e1bcdf222efedf4bcc9dc
Vinicius Guilherme Batista <62727555+Vini-Dev-Py@users.noreply.github.com>
2021-11-10 12:04:03 -0300

    Tipos e Zeros

---
+++ Curso/Fundamentos/OsZeros/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b float64
+	var c bool
+	var d string
+	var e *int
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+}
